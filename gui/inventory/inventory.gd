9946bc164cbc41c3b93abb549c3b4f54d0ac00ed
Francis Poon <francool97@gmail.com>
2024-12-09 00:20:52 -0500

    Added some utility functions to inventory

--- gui/inventory/inventory.gd
+++ gui/inventory/inventory.gd
@@ -1,11 +1,20 @@
 extends Control
 
+# TODO: how do i store a list of valid items and their ids that can be referenced? In
+# the inventory dictionary, should i store the item ids or the item name?
+#
+# basic inventory wont have item descriptions or anything, just a list of names
+# and the amount of the item in inventory
+
 signal inventory_update(item_name: StringName, item_quantity: int)
 
 # Components
 @export var gold_amount_label: Label
 
-var item_inv: Dictionary
+# dictionary that holds <item_id, count>
+var inventory_count: Dictionary
+# dictionary that holds <item_id, inventory menu item gui node>
+var inventory_menu_item: Dictionary
 var gold: int:
 	set(value):
 		gold = value
@@ -13,31 +22,57 @@ var gold: int:
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	item_inv = {}
+	inventory_count = {}
 	gold = 10
 
 	
 func _on_inventory_request(item_name: StringName):
-	var item_quantity = 0 if not item_inv.has(item_name) else item_inv[item_name]	
+	var item_quantity = 0 if not inventory_count.has(item_name) else inventory_count[item_name]	
 	inventory_update.emit(item_name, item_quantity)
 
 func _on_buy(item_name: StringName, buy_quantity: int, cost: int):
-	if not item_inv.has(item_name):
-		item_inv[item_name] = 0
+	if not inventory_count.has(item_name):
+		inventory_count[item_name] = 0
 		
 	# Also handles odd case of cost = 0
 	if buy_quantity * cost <= gold:
 		gold -= buy_quantity * cost
-		item_inv[item_name] += buy_quantity
+		inventory_count[item_name] += buy_quantity
 	else:
 		var affordable_quantity = int(gold/cost)
 		gold -= affordable_quantity * cost
-		item_inv[item_name] += affordable_quantity
+		inventory_count[item_name] += affordable_quantity
 	
 func _on_sell(item_name: StringName, sell_quantity: int, cost: int):
-	if not item_inv.has(item_name):
-		item_inv[item_name] = 0
+	if not inventory_count.has(item_name):
+		inventory_count[item_name] = 0
 		
-	var amount_sold = sell_quantity if sell_quantity <= item_inv[item_name] else item_inv[item_name]
+	var amount_sold = sell_quantity if sell_quantity <= inventory_count[item_name] else inventory_count[item_name]
 	gold += amount_sold * cost
-	item_inv[item_name] -= amount_sold
+	inventory_count[item_name] -= amount_sold
+	
+
+# Returns dictionary that holds item ids and count
+func get_all_items():
+	return inventory_count.duplicate(true)
+
+# Returns the count of item by id
+func get_item_count(id):
+	if inventory_count.has(id):
+		return inventory_count[id]
+	return 0
+
+func add_item(id, count):
+	if inventory_count.has(id):
+		inventory_count[id] += count
+	else:
+		inventory_count[id] = count
+		
+	if inventory_count[id] <= 0:
+		inventory_count.erase(id)
+
+func remove_item(id, count):
+	if inventory_count.has(id):
+		inventory_count[id] -= count
+		if inventory_count[id] <= 0:
+			inventory_count.erase(id)
