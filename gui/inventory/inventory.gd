74638c6dd4e87b37e879003dc1de22acc90b7cf2
Francis Poon <francool97@gmail.com>
2024-12-09 00:22:33 -0500

    more util functions

--- gui/inventory/inventory.gd
+++ gui/inventory/inventory.gd
@@ -12,7 +12,7 @@ signal inventory_update(item_name: StringName, item_quantity: int)
 @export var gold_amount_label: Label
 
 # dictionary that holds <item_id, count>
-var inventory_count: Dictionary
+var _inventory_count: Dictionary
 # dictionary that holds <item_id, inventory menu item gui node>
 var inventory_menu_item: Dictionary
 var gold: int:
@@ -22,57 +22,57 @@ var gold: int:
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	inventory_count = {}
+	_inventory_count = {}
 	gold = 10
 
 	
 func _on_inventory_request(item_name: StringName):
-	var item_quantity = 0 if not inventory_count.has(item_name) else inventory_count[item_name]	
+	var item_quantity = 0 if not _inventory_count.has(item_name) else _inventory_count[item_name]	
 	inventory_update.emit(item_name, item_quantity)
 
 func _on_buy(item_name: StringName, buy_quantity: int, cost: int):
-	if not inventory_count.has(item_name):
-		inventory_count[item_name] = 0
+	if not _inventory_count.has(item_name):
+		_inventory_count[item_name] = 0
 		
 	# Also handles odd case of cost = 0
 	if buy_quantity * cost <= gold:
 		gold -= buy_quantity * cost
-		inventory_count[item_name] += buy_quantity
+		_inventory_count[item_name] += buy_quantity
 	else:
 		var affordable_quantity = int(gold/cost)
 		gold -= affordable_quantity * cost
-		inventory_count[item_name] += affordable_quantity
+		_inventory_count[item_name] += affordable_quantity
 	
 func _on_sell(item_name: StringName, sell_quantity: int, cost: int):
-	if not inventory_count.has(item_name):
-		inventory_count[item_name] = 0
+	if not _inventory_count.has(item_name):
+		_inventory_count[item_name] = 0
 		
-	var amount_sold = sell_quantity if sell_quantity <= inventory_count[item_name] else inventory_count[item_name]
+	var amount_sold = sell_quantity if sell_quantity <= _inventory_count[item_name] else _inventory_count[item_name]
 	gold += amount_sold * cost
-	inventory_count[item_name] -= amount_sold
+	_inventory_count[item_name] -= amount_sold
 	
 
 # Returns dictionary that holds item ids and count
 func get_all_items():
-	return inventory_count.duplicate(true)
+	return _inventory_count.duplicate(true)
 
 # Returns the count of item by id
 func get_item_count(id):
-	if inventory_count.has(id):
-		return inventory_count[id]
+	if _inventory_count.has(id):
+		return _inventory_count[id]
 	return 0
 
 func add_item(id, count):
-	if inventory_count.has(id):
-		inventory_count[id] += count
+	if _inventory_count.has(id):
+		_inventory_count[id] += count
 	else:
-		inventory_count[id] = count
+		_inventory_count[id] = count
 		
-	if inventory_count[id] <= 0:
-		inventory_count.erase(id)
+	if _inventory_count[id] <= 0:
+		_inventory_count.erase(id)
 
 func remove_item(id, count):
-	if inventory_count.has(id):
-		inventory_count[id] -= count
-		if inventory_count[id] <= 0:
-			inventory_count.erase(id)
+	if _inventory_count.has(id):
+		_inventory_count[id] -= count
+		if _inventory_count[id] <= 0:
+			_inventory_count.erase(id)
