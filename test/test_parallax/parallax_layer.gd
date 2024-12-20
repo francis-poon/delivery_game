bd096a7d14ff044a3faa7c66132fb43f8da5a157
Francis Poon <francool97@gmail.com>
2024-12-20 00:58:10 -0500

    More parallax scene testing

--- test/test_parallax/parallax_layer.gd
+++ test/test_parallax/parallax_layer.gd
@@ -4,10 +4,11 @@ extends ParallaxLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var calc_scale = get_viewport_rect().size / target.get_rect().size
-	var target_scale = max(calc_scale.x, calc_scale.y)
-	target.scale = Vector2(target_scale, target_scale)
-	motion_mirroring = target.get_rect().size * target.scale
+	for child in get_children():	
+		var calc_scale = get_viewport_rect().size / child.get_rect().size
+		var target_scale = max(calc_scale.x, calc_scale.y)
+		child.scale = Vector2(target_scale, target_scale)
+		motion_mirroring = target.get_rect().size * target.scale
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
