24201565fc90372c379099443b7f52e001fd9dc1
Francis Poon <francool97@gmail.com>
2024-12-20 18:30:11 -0500

    More parallax testing. Started updating mining stage to have parallax

--- test/test_parallax/parallax_layer.gd
+++ test/test_parallax/parallax_layer.gd
@@ -1,15 +1,12 @@
 extends ParallaxLayer
 
-@export var target: Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for child in get_children():	
+	for child in get_children():
+		if not child is Sprite2D:
+			break
 		var calc_scale = get_viewport_rect().size / child.get_rect().size
 		var target_scale = max(calc_scale.x, calc_scale.y)
 		child.scale = Vector2(target_scale, target_scale)
-		motion_mirroring = target.get_rect().size * target.scale
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		motion_mirroring = child.get_rect().size * child.scale
